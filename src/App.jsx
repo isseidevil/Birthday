714069de14360e15ff464ea759a6c3b72d28dd2d
issei hyoudou <shadabking72@gmail.com>
2024-03-17 12:36:39 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import User from './Project/List'
+import './Project/List.css'
+
+const App = () => {
+  const[data,setData]=useState(User)
+  return (
+    <div>
+      <section className='cont'>
+      <p><h1>Today Birthdays: {data.length}</h1></p>
+      <br />
+      {
+        data.map((x)=>{
+          return(
+            
+            <div id='set'>                
+                <img src={x.image} />
+                <div>
+                  <p id='txt'>{x.name}</p>
+                  <p>{x.b_date}</p>
+                </div>
+            </div>
+            
+          )
+        })
+        
+      }
+      <button onClick={()=>setData([])}>Clear</button>
+      </section>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Project/List.js
@@ -0,0 +1,34 @@
+const User = [
+    {
+        "id": 1,
+        "name": "Issei Setou",
+        "b_date":"Apr/16",
+        "image":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTSrdsymCmc07JyzWeHTRiGtIG5FMRPQCX-YAN08qcV6g&s"
+    },
+    {
+        "id": 2,
+        "name": "Shadow Sama",
+        "b_date":"Feb/26",
+        "image":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuAZqR4mgKsCgvqqcVhQMBZPw1qWtAUQ1mXqgRLpcMdA&s"
+    },
+    {
+        "id": 3,
+        "name": "Rias Gremory",
+        "b_date":"Oct/29",
+        "image":"https://i.pinimg.com/474x/e7/96/03/e796033cb8ffe9e92b426f04f5374fad.jpg"
+    },
+    {
+        "id": 4,
+        "name": "Mr.Halloween",
+        "b_date":"Nov/31",
+        "image":"https://cdn.imgchest.com/files/l4necwaap24.png"
+    },
+    {
+        "id": 5,
+        "name": "Jack o lantern",
+        "b_date":"Aug/14",
+        "image":"https://wallpapers.com/images/hd/injured-jack-o-lantern-halloween-pfp-l8a7538yz6esf4as.jpg"
+    }
+]
+
+export default User
